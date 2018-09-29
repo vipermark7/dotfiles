c703a19bd873ba57c62d5bdfa9af481b02b8c96e
Shaffan Mustafa <shaffan1996@gmail.com>
2018-09-29 19:27:47 -0400

    worked on emacs init

--- init.el
+++ init.el
@@ -3,15 +3,25 @@
 (setq project-default-licence "GPL")
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;; load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
 (add-to-list 'package-archives '("marmalade". "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu". "http://elpa.gnu.org/packages/"))
 (cua-mode t)
 '(inhibit-startup-screen t)
-(setq initial-frame-alist '((top . 30) (left . 700) (width . 212) (height . 81)))
+(setq initial-frame-alist '(
+			    (top . 30)
+			    (left . 700)
+			    (width . 212)
+			    (height . 81)))
 
 
 (package-initialize)
-(setq show-paren-mode 1)
 ;; Set your lisp system and, optionally, some contribs
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
@@ -28,19 +38,18 @@
     (ace-window ace-link ace-jump-helm-line ac-cider auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap column-enforce-mode clomacs clean-aindent-mode base16-theme evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump diminish define-word evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround expand-region exec-path-from-shell evil-visualstar golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse helm-flx helm-descbinds helm-cider helm-ag google-translate helm-themes helm-swoop helm-projectile helm-mode-manager helm-make highlight-parentheses highlight-numbers highlight-indentation linum-relative link-hint indent-guide hungry-delete hl-todo open-junk-file neotree multi-term move-text lorem-ipsum popwin persp-mode pcre2el paradox org-plus-contrib org-bullets toc-org spaceline slime restart-emacs request rainbow-delimiters which-key volatile-highlights vi-tilde-fringe uuidgen use-package gruvbox-theme gruvbox-theme paredit rainbow-identifiers ws-butler winum))))
 (package-refresh-contents)
 (package-install-selected-packages)
-<<<<<<< HEAD
 (require 'evil)
 (require 'paredit)
+(evil-mode 1)
+(paredit-mode 1) 
 
+
+(visual-line-mode 1)
 '(show-paren-mode t)
 (setq show-paren-style 'expression)
 (electric-pair-mode 1)
 (load-theme 'gruvbox-dark-medium 'no-confirm)
-(evil-mode 1)
-(paredit-mode 1) 
 
-(require 'evil)
-(require 'paredit)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
